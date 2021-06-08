424f47d51a8a217356d0b25359dbc12d85c5640b
zestybean <brando.lugo@gmail.com>
2021-06-08 11:37:00 -0500

    Initial Commit

---
+++ Shared/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Shared
+//
+//  Created by Brando Lugo on 6/8/21.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
