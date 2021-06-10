4067f8817504ce32ee9bb0c7e88b683a7524cc1b
zestybean <brando.lugo@gmail.com>
2021-06-10 16:31:26 -0500

    Implemented nabber with animated logo

---
+++ Shared/View/Home/LogoView.swift
@@ -0,0 +1,39 @@
+//
+//  LogoView.swift
+//  Touchdown
+//
+//  Created by Brando Lugo on 6/10/21.
+//
+
+import SwiftUI
+
+struct LogoView: View {
+    //MARK: - PROPS
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack(spacing: 4) {
+            Text("Touch".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+            Image("logo-dark")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30, alignment: .center)
+            Text("Down".uppercased())
+                .font(.title3)
+                .fontWeight(.black)
+                .foregroundColor(.black)
+        }
+    }
+}
+
+//MARK: - PREV
+struct LogoView_Previews: PreviewProvider {
+    static var previews: some View {
+        LogoView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
---
+++ Shared/View/Home/NavigationBarView.swift
@@ -0,0 +1,58 @@
+//
+//  NavigationBarView.swift
+//  Touchdown
+//
+//  Created by Brando Lugo on 6/9/21.
+//
+
+import SwiftUI
+
+struct NavigationBarView: View {
+    //MARK: - PROPS
+    @State private var isAnimated: Bool = false
+    
+    //MARK: - BODY
+    var body: some View {
+        HStack {
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                Image(systemName: "magnifyingglass")
+                    .font(.title)
+                    .foregroundColor(.black)
+            })//BUTTON
+            
+            Spacer()
+            
+            LogoView()
+                .opacity(isAnimated ? 1: 0)
+                .offset(x: 0, y: isAnimated ? 0 : -25)
+                .onAppear(perform: {
+                    withAnimation(.easeOut(duration: 0.5)){
+                        isAnimated.toggle()
+                    }
+                })
+            
+            Spacer()
+            
+            Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/, label: {
+                ZStack {
+                    Image(systemName: "cart")
+                        .font(.title)
+                        .foregroundColor(.black)
+                    Circle()
+                        .fill(Color.red)
+                        .frame(width: 14, height: 14, alignment: .center)
+                        .offset(x: 13, y: -10)
+                }
+            })//BUTTON
+        }//HSTACK
+    }
+}
+
+//MARK: - PREV
+struct NavigationBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationBarView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
